7bb62797e41a6f8c77ddb513c6592164170d2344
nshroff <nshroff@HFCDT5363>
2017-10-04 14:29:25 -0400

    Initical database checkin

---
+++ dbo/Tables/Employees.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeId] INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]  NVARCHAR (MAX) NULL,
+    [LastName]   NVARCHAR (MAX) NULL,
+    [Title]      NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
